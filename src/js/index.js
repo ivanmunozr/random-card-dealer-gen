deb25bb0891c27cff55a139fa1f77cc1bab06202
Ivan Muñoz <ivan.munoz.r1@outlook.com>
2019-09-13 00:19:22 +0000

    ejercicio

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,29 @@ import "file-loader?name=[name].[ext]!../index.html";
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pintas = ["&diams;", "&hearts;", "&clubs;", "&spades;"];
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  function buscar(num) {
+    let x = Math.floor(Math.random() * num);
+    return x;
+  }
+  let variable = pintas[buscar(pintas.length)];
+  if (variable == "&diams;" || variable == "&hearts;") {
+    document.getElementById("pinta-up").style.color = "red";
+    document.getElementById("pinta-down").style.color = "red";
+    document.getElementById("pinta-up").innerHTML = variable;
+    document.getElementById("pinta-down").innerHTML = variable;
+    document.getElementById("numero").innerHTML =
+      numeros[buscar(numeros.length)];
+    document.getElementById("numero").style.color = "red";
+  } else {
+    document.getElementById("pinta-up").style.color = "black";
+    document.getElementById("pinta-down").style.color = "black";
+    document.getElementById("pinta-up").innerHTML = variable;
+    document.getElementById("pinta-down").innerHTML = variable;
+    document.getElementById("numero").innerHTML =
+      numeros[buscar(numeros.length)];
+  }
 };
